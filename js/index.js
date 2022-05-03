37c9f82e9260cb8d00d5ee09f6c40e88291eb916
anchounio <toni.clemente.garcia@gmail.com>
2022-05-04 00:51:44 +0200

    Working functionality

--- js/index.js
+++ js/index.js
@@ -7,31 +7,32 @@ let div = document.querySelector('div');
 // Array de emojis
 
 let emojiList = [
-    '💀',
-    '💀',
-    '👺',
-    '👺',
-    '🤡',
-    '🤡',
-    '🎅🏻',
-    '🎅🏻',
-    '👽',
-    '👽',
-    '🎃',
-    '🎃',
-    '👾',
-    '👾',
-    '🤖',
-    '🤖',
+    { emoji: '💀', matched: false },
+    { emoji: '💀', matched: false },
+    { emoji: '👺', matched: false },
+    { emoji: '👺', matched: false },
+    { emoji: '🤡', matched: false },
+    { emoji: '🤡', matched: false },
+    { emoji: '🎅🏻', matched: false },
+    { emoji: '🎅🏻', matched: false },
+    { emoji: '👽', matched: false },
+    { emoji: '👽', matched: false },
+    { emoji: '🎃', matched: false },
+    { emoji: '🎃', matched: false },
+    { emoji: '👾', matched: false },
+    { emoji: '👾', matched: false },
+    { emoji: '🤖', matched: false },
+    { emoji: '🤖', matched: false },
 ];
 
 let emojiListRandom = [];
 
 function GenerateCard(emoji) {
     const div = document.createElement('div');
-    div.innerHTML = `<div class="hidden " alt="${emoji}">${emojiList[emoji]} </div>`;
+    div.innerHTML = `<div class="hidden " data-number="${emojiListRandom}">${emojiList.emoji} </div>`;
     cardContainer.append(div);
     console.log(div.textContent);
+    console.log(emojiList);
 }
 
 function GenerateCardboard() {
@@ -51,37 +52,42 @@ function GenerateCardboard() {
 }
 
 function card1() {
-    div.removeEventListener('click', card1);
-    div.removeEventListener('click', card2);
-    let cosa1 = div.addEventListener('click', (e) => {
+    let div1 = document.querySelector('div');
+    div1.removeEventListener('click', card1);
+    let cosa1 = div1.addEventListener('click', (e) => {
         console.log('estoy en card1');
         // 1. Seleccionamos de forma inequívoca el elemento sobre el cuál hemos
         // hecho click.
+
         let clickedDiv1 = '';
+        console.log(e.target);
         clickedDiv1 = e.target;
-        console.log(console.log('card1: ', clickedDiv1));
+        if (clickedDiv1.className === 'untouchable') {
+            card1();
+        } else {
+            console.log(console.log('card1: ', clickedDiv1));
 
-        clickedDiv1.className = 'emoji';
+            clickedDiv1.className = 'emoji';
 
-        card2(clickedDiv1, cosa1);
+            card2(clickedDiv1, cosa1);
+        }
     });
-    div.removeEventListener('click', card1);
-    div.removeEventListener('click', card2);
+    div1.removeEventListener('click', card1);
 }
 
 function card2(clickedDiv1) {
-    div.removeEventListener('click', card1);
-    div.removeEventListener('click', card2);
-    let cosa2 = div.addEventListener('click', (e) => {
+    let div2 = document.querySelector('div');
+    div2.removeEventListener('click', card2);
+    let cosa2 = div2.addEventListener('click', (e) => {
         console.log('estoy en card2');
         let clickedDiv2 = '';
         clickedDiv2 = e.target;
         console.log('card2: ', clickedDiv2);
-
         clickedDiv2.className = 'emoji';
         if (clickedDiv2.textContent === clickedDiv1.textContent) {
-            clickedDiv1.className = 'emoji';
-            clickedDiv2.className = 'emoji';
+            clickedDiv1.className = 'untouchable';
+            clickedDiv2.className = 'untouchable';
+
             card1();
         } else if (clickedDiv2.textContent !== clickedDiv1.textContent) {
             clickedDiv1.className = 'hidden';
@@ -89,8 +95,8 @@ function card2(clickedDiv1) {
             card1();
         }
     });
-    div.removeEventListener('click', card1);
-    div.removeEventListener('click', card2);
+
+    div2.removeEventListener('click', card2);
 }
 
 GenerateCardboard();
